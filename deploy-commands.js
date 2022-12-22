b04ca44f4e21894e4659865904756485de74835a
c-l-scholl <cscholl@macalester.edu>
2022-12-21 16:18:57 -0800

    commands appear in discord now

--- deploy-commands.js
+++ deploy-commands.js
@@ -22,7 +22,7 @@ const rest = new REST({ version: '10' }).setToken(token);
 		console.log(`Start refreshing ${commands.length} application (/) commands.`);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
-		const data = rest.put(
+		const data = await rest.put(
 			Routes.applicationGuildCommands(clientId, guildId),
 			{ body: commands },
 		);
